011e9d661f4b21fd76045a87bb344952038a6d41
z1z1z1joey <z1z1z1joey@gmail.com>
2017-12-02 21:14:10 +0800

    add restaurant index action and view for admin

---
+++ app/helpers/admin/reataurants_helper.rb
@@ -0,0 +1,2 @@
+module Admin::ReataurantsHelper
+end
---
+++ test/controllers/admin/reataurants_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::ReataurantsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
