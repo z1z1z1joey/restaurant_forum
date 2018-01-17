e036f449a9eebb496e5f599b0fde736ad87fbe17
z1z1z1joey <z1z1z1joey@gmail.com>
2018-01-17 18:45:35 +0800

    you can see the restaurant you have already commented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
    before_action :set_user, only: [:show, :edit, :update]
   def show
-   
+    @commented_restaurants = @user.restaurants.uniq
   end
   def edit
     
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:restaurant).permit(:name, :intro, :avatar)  
+    params.require(:user).permit(:name, :intro, :avatar)  
   end
 
   def set_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  mount_uploader :avatar, AvatarUploader
   has_many :comments
+  has_many :restaurants, through: :comments
 
   def admin?
     self.role == "admin"
