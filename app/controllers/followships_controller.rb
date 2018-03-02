5976ef1d5942159b5af0dc930f9ed7360aef6b92
z1z1z1joey <z1z1z1joey@gmail.com>
2018-03-03 01:21:51 +0800

    依狀態顯示 follow / unfollow 按鈕

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -3,11 +3,19 @@ class FollowshipsController < ApplicationController
     @followship = current_user.followships.build(following_id: params[:following_id])
 
     if @followship.save
-      flash[:notice] = "Sucessfully followed"
+      flash[:notice] = "Successfully followed"
       redirect_back(fallback_location: root_path)
     else
-      flash[:alert] = @followship.errors.full_message.to_sentence
+      flash[:alert] = @followship.errors.full_messages.to_sentence
       redirect_back(fallback_location: root_path)
-    end    
+    end
+  end
+
+  def destroy
+    # where 會回傳物件集合(Array)，需要串接可取出單一物件的方法如 first
+    @followship = current_user.followships.where(following_id: params[:id]).first
+    @followship.destroy
+    flash[:alert] = "Followship destroyed"
+    redirect_back(fallback_location: root_path)
   end
 end
