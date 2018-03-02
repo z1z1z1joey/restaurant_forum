3570d8ffa4cf280e0db13c8cd5589bfb64c9d9e6
z1z1z1joey <z1z1z1joey@gmail.com>
2018-03-03 00:25:30 +0800

    add user index page with follow button,validate uniqueness of following_id in user_id scope,add create action for followship record

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,2 +1,13 @@
 class FollowshipsController < ApplicationController
+  def create
+    @followship = current_user.followships.build(following_id: params[:following_id])
+
+    if @followship.save
+      flash[:notice] = "Sucessfully followed"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @followship.errors.full_message.to_sentence
+      redirect_back(fallback_location: root_path)
+    end    
+  end
 end
